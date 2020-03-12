39840471560ac855f8300a28626a84724a81bda1
Mango Sánchez Redondo <eduardo.sanchezredondo@gmail.com>
2020-03-11 22:58:56 -0300

    created domain project and entities related with json data

---
+++ MNG.All/MNG.Domain/Entities/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MNG.Domain.Entities
+{
+    public class Client
+    {
+        [JsonProperty("id")]
+        public string IdClient { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("role")]
+        public string Role { get; set; }
+    }
+}
---
+++ MNG.All/MNG.Domain/Entities/Policy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MNG.Domain.Entities
+{
+    public class Policy
+    {
+        [JsonProperty("id")]
+        public string IdPolicy { get; set; }
+
+        [JsonProperty("clientId")]
+        public string IdClient { get; set; }
+
+        [JsonProperty("amountInsured")]
+        public decimal AmountInsured { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("inceptionDate")]
+        public DateTime InceptionDate { get; set; }
+
+        [JsonProperty("installmentPayment")]
+        public bool InstallmentPayment { get; set; }
+    }
+}
